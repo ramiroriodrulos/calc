e092bd07790761f9e777853f269c7dcb2a984158
Valentin Petti <valenpetti46@gmail.com>
2022-04-15 17:33:26 -0300

    Se agrega función div() - División

--- fns.js
+++ fns.js
@@ -24,6 +24,11 @@ function mul(a, b) {
 function div(a, b) {
     // Tener en cuenta division por 0
     // Si b es 0 retornar "Error: div by 0"
+    if (b == 0) {
+        return "Error: div by 0";
+    }else{
+        return a / b;
+    }
 }
 
 function pow(a, b) {
